8c607ff41d289345ed173a8c990d9becff3b3c32
Trammell hudson <hudson@trmm.net>
2021-02-06 00:46:22 +0100

    strokes per minute, integrated power, force, etc

--- row.ino
+++ row.ino
@@ -16,8 +16,8 @@
  * Reversing on the same edge?
  * Weird blip in the center of the window.
  */
-#define QUAD_A 14
-#define QUAD_B 12
+#define QUAD_A 12
+#define QUAD_B 14
 
 #define LED_A 0
 #define LED_B 2
@@ -26,10 +26,10 @@
 void setup()
 {
 	Serial.begin(115200);
-	//pinMode(QUAD_A, INPUT_PULLUP);
-	//pinMode(QUAD_B, INPUT_PULLUP);
-	pinMode(QUAD_A, INPUT);
-	pinMode(QUAD_B, INPUT);
+	pinMode(QUAD_A, INPUT_PULLUP);
+	pinMode(QUAD_B, INPUT_PULLUP);
+	//pinMode(QUAD_A, INPUT);
+	//pinMode(QUAD_B, INPUT);
 
 	pinMode(LED_A, OUTPUT);
 	pinMode(LED_B, OUTPUT);
@@ -45,8 +45,15 @@ int rise_b;
 
 int do_output;
 
+unsigned start_usec;
+unsigned stroke_power;
+int delta_usec;
+int last_delta_usec;
+unsigned spm;
+
 void loop()
 {
+	int got_tick = 0;
 	const int a = digitalRead(QUAD_A);
 	const int b = digitalRead(QUAD_B);
 	const unsigned now = micros();
@@ -56,7 +63,7 @@ void loop()
 
 	if (a && b)
 	{
-		// back to idle, so prep for the next output
+		// back to idle, so prep for the next tick output
 		do_output = 1;
 	}
 
@@ -65,59 +72,82 @@ void loop()
 		if (now - rise_a > 20000)
 			fall_a = now;
 
-/*
-		Serial.print(now);
-		Serial.println(" A-");
-*/
-
 		if (!b && do_output)
 		{
 			// b is already low, so this is a negative speed
 			do_output = 0;
-			Serial.print(now);
-			Serial.print(" ");
-			Serial.println((int)(fall_b - now));
+			got_tick = 1;
+			delta_usec = fall_b - now;
 		}
 	}
 			
 	if (a && !last_a)
 	{
 		rise_a = now;
-/*
-		Serial.print(now);
-		Serial.println(" A+");
-*/
 	}
 
 	if (!b && last_b)
 	{
 		if (now - rise_b > 20000)
 			fall_b = now;
-/*
-		Serial.print(now);
-		Serial.println(" B-");
-*/
 
 		if (!a && do_output)
 		{
 			// a is already low, so this is a positive speed
 			do_output = 0;
-			digitalWrite(LED_B, 1);
-			Serial.print(now);
-			Serial.print(" ");
-			Serial.println((int)(now - fall_a));
+			got_tick = 1;
+			delta_usec = now - fall_a;
 		}
 	}
 
 	if (b && !last_b)
 	{
 		rise_b = now;
-/*
-		Serial.print(now);
-		Serial.println(" B-");
-*/
 	}
 
 	last_a = a;
 	last_b = b;
+
+	if (!got_tick)
+		return;
+
+	// we have received a new data point,
+	// process it and output the update on the serial port
+
+	// need a scaling factor to compute how much
+	// force they are applying.
+	const int force = 1000000000L / delta_usec;
+
+	// on positive velocity pulls, check for a sign change
+	// for tracking power and distance
+	if (delta_usec > 0)
+	{
+		if (last_delta_usec < 0)
+		{
+			// sign change: this is the start of a new stroke
+			const unsigned stroke_delta = now - start_usec;
+			spm = (600000000L / stroke_delta + spm * 3) / 4;
+			
+			start_usec = now;
+			stroke_power = 0;
+
+			// blank line to mark the log
+			Serial.println();
+		}
+
+		stroke_power += force;
+	}
+
+	last_delta_usec = delta_usec;
+
+	Serial.print(now);
+	Serial.print(" ");
+	Serial.print(now - start_usec);
+	Serial.print(" ");
+	Serial.print(force);
+	Serial.print(" ");
+	Serial.print(delta_usec > 0 ? stroke_power : 0);
+	Serial.print(" ");
+	Serial.print(spm);
+	Serial.println();
 }
