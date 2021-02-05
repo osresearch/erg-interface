2cbdef2949b0d99023dfed5a121661e107beb71d
Holly Hudson <commerce+gitlab@hollyhudson.net>
2021-02-06 00:53:06 +0100

    updates readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 
-Adds an interface for the Virtufit 1000 rowing machine to provide custom stats.  In extreme early development stage.
+Adds an interface for the [VIRTUFIT WATER RESISTANCE ROW 1000](https://virtufit.nl/product/virtufit-foldable-water-resistance-row-1000-roeitrainer/) rowing machine to provide custom stats.  In extreme early development stage.
--- erg_code/erg_code.ino
+++ erg_code/erg_code.ino
@@ -1,7 +1,7 @@
-const int RED_LED = ;
-const int GREEN_LED = ; 
-const int DRIVE_PIN = ; // lights first during the drive
-const int RECOVERY_PIN = ; // lights first during the recovery
+const int RED_LED = 14;
+const int GREEN_LED = 12; 
+//const int DRIVE_PIN = ; // lights first during the drive
+//const int RECOVERY_PIN = ; // lights first during the recovery
 
 void setup() {
 	// put your setup code here, to run once:
@@ -14,7 +14,7 @@ void loop() {
 	digitalWrite(RED_LED, HIGH);
 	delay(300);
 	digitalWrite(GREEN_LED, HIGH);
-	delay(600);
+	delay(1200);
 	digitalWrite(RED_LED, LOW);
 	delay(300);
 	digitalWrite(GREEN_LED, LOW);
