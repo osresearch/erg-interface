fb6d90fc14eba5f02476a350d3ed4e40b0327655
Trammell hudson <hudson@trmm.net>
2021-02-06 17:58:33 +0100

    add power strip chart, fix power computation

--- workout/sketch.js
+++ workout/sketch.js
@@ -2,7 +2,10 @@
  * Make a websocket connection to the rower and then draw the data
  */
 
+let new_data = null;
 let data = [];
+let total_work = 0;
+let last_work = 0;
 
 function setup()
 {
@@ -22,14 +25,22 @@ function setup()
 		if (e.data == "Connected")
 			return;
 
-		let cols = e.data.split(',');
-		data.push(cols);
+		new_data = e.data.split(',');
+
+		// fix up the spm colun
+		new_data[4] /= 10.0;
+
+		// fake out the last power measurement, just in case
+		if (new_data[1] == 0)
+		{
+			last_work = int(total_work);
+			new_data[3] = int(total_work);
+		}
 	};
 }
 
 let last_sx = 0;
 let last_sy = 0;
-let total_work = 0;
 
 function draw_stroke(d,w,h)
 {
@@ -37,7 +48,7 @@ function draw_stroke(d,w,h)
 	// the start of a new stroke.  decay the old chart a little bit
 	if (d[1] == 0) {
 		noStroke();
-		fill(0, 0, 0, 5);
+		fill(0, 0, 0, 35);
 		rect(0, 0, w, h);
 		last_sx = 0;
 		last_sy = 0;
@@ -49,7 +60,7 @@ function draw_stroke(d,w,h)
 		line(0,h/2, w, h/2);
 	}
 
-	let vel = d[2] / 1e4;
+	let vel = d[2] / 5000;
 	if (vel > 0)
 	{
 		let force = vel * vel;
@@ -61,7 +72,7 @@ function draw_stroke(d,w,h)
 	strokeWeight(2);
 	stroke(0,255,0);
 	let sx = d[1] * w / 3e6; // up to 3 seconds of display
-	let sy = -vel * h/2 / 5;
+	let sy = -vel * h/2 / 10;
 
 	if (sx > w)
 		sx = w;
@@ -84,13 +95,13 @@ function draw_stroke(d,w,h)
 	text(int(total_work), w, 50);
 }
 
-function draw_strip(d,w,h)
+function draw_strip(d,w,h,index,max,scale_step)
 {
 	let now = d[0];
 	let start = data[0][0];
 	let delta = now - start;
 	let sx = w / (delta+1);
-	let sy = h / 60.0;
+	let sy = h / max;
 
 	fill(0);
 	rect(0,0,w,h);
@@ -98,13 +109,8 @@ function draw_strip(d,w,h)
 	noFill();
 	stroke(50);
 	strokeWeight(1);
-	line(0, h -  0*sy, w, h -  0*sy);
-	line(0, h - 10*sy, w, h - 10*sy);
-	line(0, h - 20*sy, w, h - 20*sy);
-	line(0, h - 30*sy, w, h - 30*sy);
-	line(0, h - 40*sy, w, h - 40*sy);
-	line(0, h - 50*sy, w, h - 50*sy);
-	line(0, h - 60*sy, w, h - 60*sy);
+	for(let v = 0 ; v < max ; v += scale_step)
+		line(0, h -  v*sy, w, h -  v*sy);
 
 	for(let t = 15e6 ; t < delta ; t += 15 * 1e6)
 		line(t*sx, 0, t*sx, h);
@@ -121,34 +127,50 @@ function draw_strip(d,w,h)
 		if (p[1] != 0)
 			continue;
 		let x = (p[0] - start) * sx;
-		let y = (p[4] / 10.0) * sy;
+		let y = p[index] * sy;
 		vertex(x, h - y);
 	}
 
 	endShape();
-
-	fill(0xff);
-	noStroke();
-	textSize(50);
-	textAlign(RIGHT, BOTTOM);
-	text(int(delta / 1e6), w, 50);
-	text(d[4] / 10.0, w, 100);
-
 }
 
 function draw()
 {
 	// retrieve the most recent data
-	let d = data[data.length-1];
-	if (!d)
+	if (!new_data)
 		return;
+	data.push(new_data);
 
 	push();
-	draw_stroke(d, 400, 200);
+	translate(0,0);
+	draw_stroke(new_data, 400, 400);
 	pop();
 
+	// draw the strokes per minute
 	push();
 	translate(400,0);
-	draw_strip(d, width-400, 200);
+	draw_strip(new_data, width-400, 200, 4, 60, 10);
+
+	fill(0xff);
+	noStroke();
+	textSize(50);
+	textAlign(RIGHT, BOTTOM);
+	text(new_data[4], width-400, 100);
+
 	pop();
+
+	// draw the power per minute
+	push();
+	translate(400,200);
+	draw_strip(new_data, width-400, 200, 3, 1000, 100);
+
+	fill(0xff);
+	noStroke();
+	textSize(50);
+	textAlign(RIGHT, BOTTOM);
+	text(int(last_work), width-400, 100);
+
+	pop();
+
+	new_data = null;
 }
